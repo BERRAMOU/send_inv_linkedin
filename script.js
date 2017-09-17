e78336695fc9ccb292f247a193ea99f2475b7f2f
BERRAMOU2017 <azz-eddine.BERRAMOU@edu.uca.ac.ma>
2017-09-18 00:34:11 +0100

    fix

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-// function click_button(i) {
-//   var ul = document.getElementsByClassName('mn-pymk-list__cards')[0];
-//   var items = ul.getElementsByTagName('li');
-//   var connect_div;
-//   var connect_button;
-//   connect_div = items[i].getElementsByClassName('mn-pymk-list__action-container');
-//   connect_button = connect_div[0].getElementsByClassName('button-secondary-small');
-//   connect_button[0].click();
-//
-//   console.log('clicked ');
-//
-// }
-
-function loop_for_element() {
-
-  var j = 0;
-
-  var ul = document.getElementsByClassName('mn-pymk-list__cards')[0];
-  var items = ul.getElementsByTagName('li');
-
-  while (j < items.length) {
-
-    var connect_div;
-    var connect_button;
-    connect_div = items[j].getElementsByClassName('mn-pymk-list__action-container');
-    connect_button = connect_div[0].getElementsByClassName('button-secondary-small');
-    connect_button[0].click();
-    console.log(j + ' button clicked ');
-    sleep(5000);
-    j++;
-
-  }
-
-  document.body.scrollTop = document.body.scrollHeight;
-}
-
 function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
@@ -43,4 +7,33 @@ function sleep(milliseconds) {
   }
 }
 
-window.onload = loop_for_element;
+
+var j = 0;
+var ul = document.getElementsByClassName('mn-pymk-list__cards')[0];
+var items = ul.getElementsByTagName('li');
+var connect_div;
+var connect_button;
+var name;
+
+// function loop_for_element() {
+
+while (j < items.length) {
+
+  connect_div = items[j].getElementsByClassName('mn-pymk-list__action-container');
+  connect_button = connect_div[0].getElementsByClassName('button-secondary-small');
+  name = items[j].getElementsByClassName('mn-person-info__name mn-person-info__name--card-layout Sans-15px-black-85%-semibold')[0].innerHTML;
+
+  connect_button[0].click();
+
+  console.log(j + ' :  invitation sent to  ' + name);
+
+  sleep(1000);
+  j++;
+}
+
+document.body.scrollTop = document.body.scrollHeight;
+
+sleep(15000);
+
+// }
+
